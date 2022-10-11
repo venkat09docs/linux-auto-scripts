e128c3d192fce9d9d57aab2b7da8f69d7b63e18b
venkat09 <venkat09docs@gmail.com>
2022-10-11 20:54:30 +0530

    Update ami-setup.sh

--- ami-setup.sh
+++ ami-setup.sh
@@ -56,7 +56,7 @@ chmod +x /etc/profile.d/ps1.sh
 Stat $? "Enable Color Prompt"
 
 ## Enable idle shutdown
-curl -s https://gitlab.com/cit-devops/intros/raw/master/scipts/idle.sh -o /boot/idle.sh
+curl -s https://raw.githubusercontent.com/venkat09docs/linux-auto-scripts/main/idle.sh -o /boot/idle.sh
 chmod +x /boot/idle.sh
 STAT1=$?
 
@@ -82,8 +82,8 @@ chattr +i /etc/ssh/sshd_config
 Stat $? "Enable Password Login"
 
 ## Setup user passwords
-ROOT_PASS="DevOps321"
-CENTOS_PASS="DevOps321"
+ROOT_PASS="devops123"
+CENTOS_PASS="devops123"
 #usermod -a -G google-sudoers centos &>/dev/null
 echo "echo $ROOT_PASS | passwd --stdin root"   >>/etc/rc.d/rc.local
 echo "echo $CENTOS_PASS | passwd --stdin centos"   >>/etc/rc.d/rc.local
@@ -93,7 +93,7 @@ info "   Following are the Usernames and Passwords"
 Infot "centos / $CENTOS_PASS"
 Infot "  root / $ROOT_PASS"
 echo
-echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIfSCB5MtXe54V3lWGBGSxMWPue5CjmSA4ky7E8GUoeZdXxI+df7msJL93PzmtwU3v+O+NLNJJRfmaGpEkgidVXoi6mnYUVCHb1y4zd6QIFEyglGDlvZ4svhHt7T15B13bJC3mTaR2A/xqlvE0/a4XKN1ATYyn6K6CTFJT8I4TIDQmO3PbcNsNFXoO1ef657aqNf0AXC1QWum3HulIt6iJ4s0pQI4hDTmR5EskJxr2K62F4JDOYmVu8bGhFT6ohYbXBCGQtmdp716RnF0Cp1htmxM001wvCSjWLPZuuBjtHXX+op+MJGr0aIqqxdVZ2gw0JeIDfVo7pkSIdTu+p2Yn devops' >/root/.ssh/authorized_keys
+# echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIfSCB5MtXe54V3lWGBGSxMWPue5CjmSA4ky7E8GUoeZdXxI+df7msJL93PzmtwU3v+O+NLNJJRfmaGpEkgidVXoi6mnYUVCHb1y4zd6QIFEyglGDlvZ4svhHt7T15B13bJC3mTaR2A/xqlvE0/a4XKN1ATYyn6K6CTFJT8I4TIDQmO3PbcNsNFXoO1ef657aqNf0AXC1QWum3HulIt6iJ4s0pQI4hDTmR5EskJxr2K62F4JDOYmVu8bGhFT6ohYbXBCGQtmdp716RnF0Cp1htmxM001wvCSjWLPZuuBjtHXX+op+MJGr0aIqqxdVZ2gw0JeIDfVo7pkSIdTu+p2Yn devops' >/root/.ssh/authorized_keys
 sed -i -e 's/showfailed//' /etc/pam.d/postlogin
 chmod +x /etc/rc.d/rc.local
 systemctl enable rc-local
@@ -109,7 +109,7 @@ chmod 600 /root/.ssh/config
 # Install AWS CLI
 cd /tmp
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip awscliv2.zip &>/dev/null
 /tmp/aws/install
 
 rm -rf /var/lib/yum/*  /tmp/*
